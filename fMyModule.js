4546784eb8013d6fea9f13fe40a59152b819f7c8
Afanasiy Zaharov <iphonya96@gmail.com>
2015-09-30 19:10:31 +0400

    i fixed controller and added some function

--- fMyModule.js
+++ fMyModule.js
@@ -4,75 +4,49 @@ var log = function(obj) {
 		//console.log(obj);
 	}
 }
-var onMessageListener = function(message, sender, sendResponse) {
-	switch(message.type) {
-        case "cookies":
-        log("response got");
-            log(message.obj);
-        break;
-    }
-    return true;
-}
-var backgroundPageConnection = chrome.runtime.connect({
-	name : "devtools-page"
-});
-var tabId = chrome.devtools.inspectedWindow.tabId;
-backgroundPageConnection.postMessage({
-		type : "getall",
-		tabId : tabId
-	});
-backgroundPageConnection.onMessage.addListener(function(message) {
-	   log(message.cks);
-});
 
-//chrome.runtime.onMessage.addListener(onMessageListener);
+
+
+
 
 window.onerror = function(err, url, number){
 	log(err);
 	log(url);
 	log(number);
 }
-var testObject = {
-	testField: "FieldIsWorking",
-	testMethod: function(){
-		log("method is working");
-		console.log("method is working");
-	}
-};
-//testObject.testMethod();
-//var abc  = chrome.devtools.inspectedWindow.tabId;
-/*log(testObject);
-console.log(testObject);*/
-//alert(angular.version.codeName);
-//log(angular.version);
 
-/*try{
-	log(Resources.url);
-}catch(e){
-	log(e.message);
-}*/
-//log(Resource.url);
 
-//chrome.devtools.inspectedWindow.eval('log();console.log("test")');
-//chrome.devtools.inspectedWindow.eval.apply(this, ['try {console.log(test)}catch(e){console.log("test")}']);
-//chrome.devtools.inspectedWindow.reload();
+
+
 var myModule  = angular.module('myModule', []);
 myModule.controller('cookieCtrl', function($scope, $timeout, cookieService){
-	$scope.test = 123;
-	$scope.doubletest = "abc";
-	$scope.a = cookieService.testing;
-	
+	$scope.test = "test";
+	$scope.cookies = []
 	log(chrome);
 	$scope.service = cookieService;
-	chrome.runtime.sendMessage({type: "getCookies"});
+	$scope.init = function(){
+		$scope.service.init();
+		$timeout(function(){
+			log($scope.service.cache);
+			for(var i = 0; i<$scope.service.cache.length; i++){
+				$scope.cookies.push($scope.service.cache[i])
+				$scope.cookies[i].selected = false;
+				$scope.cookies[i].number  = i;
+			}
+			log($scope.cookies);
 
-	$scope.service.init();
-	$timeout(function(){
-		log($scope.service.cache);
-	},500);
+		}, 300)
+		$scope.select = function(cookieNumber){
+			$scope.cookies[i].selected = true;
+
+		}
+	}
+	$scope.init();
+	
 
 
-	//chrome.devtools.inspectedWindow.eval('console.log("fmm is working")');
+
+
+
 
 });
-//log(myModule);
